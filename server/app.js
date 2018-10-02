8ef1352428d90bc7f9c7027c5af248e96e37a3df
ctiller15 <christiller2015@gmail.com>
2018-10-01 22:01:43 -0400

    Wrap requests in native es6 promises.

--- server/app.js
+++ server/app.js
@@ -1,26 +1,14 @@
-const request = require("request");
+const rp = require("request-promise-native");
 
 const getBooruData = () => {
-    request
-    .get({
+    return rp.get({
         uri: "https://www.sakugabooru.com/post.json?limit=10",
         json: true
-        }, 
-        (error, response, body) => {
-        const animeData = body.map((m) => {
-            const dataItem = {
-                author: m.author,
-                file_url: m.file_url,
-                preview_url: m.preview_url,
-                sample_url: m.sample_url,
-                jpeg_url: m.jpeg_url,
-                tags: m.tags
-            }
-    
-            return dataItem;
-        });    
-        console.log(animeData);
-    });
+        });
 }
 
-getBooruData();
+getBooruData()
+    .then((response) => {
+        console.log("I did it!!!");
+        console.log(response);
+    });
