6f6c4b91deebbfce404fd867a3804da53e877c6c
ctiller15 <christiller2015@gmail.com>
2018-10-01 22:09:42 -0400

    install expressjs and display data on screen

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const rp = require("request-promise-native");
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const getBooruData = () => {
     return rp.get({
@@ -7,8 +10,13 @@ const getBooruData = () => {
         });
 }
 
-getBooruData()
-    .then((response) => {
-        console.log("I did it!!!");
-        console.log(response);
-    });
+app.get('/', (req, res) => {
+    getBooruData()
+        .then((response) => {
+            res.send(response);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
