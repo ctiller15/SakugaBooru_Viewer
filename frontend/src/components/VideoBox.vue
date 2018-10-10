2203c6ed560a5e017e09bc9f0301f3cf68cee5fa
ctiller15 <christiller2015@gmail.com>
2018-10-09 21:00:56 -0400

    Create videobox and break videobox out to separate component.

---
+++ frontend/src/components/VideoBox.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div class="video-box" 
+         v-on:click="play_video = true">
+        <div v-if="!play_video">
+            <img v-bind:src="videoData['preview_url']">
+        </div>
+        <div v-else>
+            <video controls>
+                <source v-bind:src="videoData['jpeg_url']" type="video/mp4">
+            </video>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Videobox',
+  data: function () {
+    return {
+      play_video: false,
+    }
+  },
+  props: {
+    videoData: Object
+  }
+}
+
+</script>
